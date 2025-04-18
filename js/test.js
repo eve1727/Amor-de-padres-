aab326f952ae9bbc00bbfb10d8360d937ec3d77e
eve1727 <evelynesparza23@cbtis52.edu.mx>
2025-04-17 21:29:10 -0600

    Hice el menu, acomode footer, fonts y colorimetria

--- js/test.js
+++ js/test.js
@@ -1,3 +1,20 @@
 /* Muestra la bienvenida al sitio*/
-var bienvenida = " Hola! bienvendo a donde conoceras un poco sobre la mente adolescente";
+var bienvenida = " Hola! bienvenido a donde conoceras un poco sobre la mente adolescente";
 alert(bienvenida);
+
+// Obtiene el botón con el ID 'hamburger-btn' y agrega un evento de escucha al hacer clic
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+
+    // Obtiene el elemento del menú lateral con el ID 'side-menu'
+    var sideMenu = document.getElementById('side-menu');
+
+    // Alterna la clase 'active' en el menú lateral
+    // Si la clase 'active' está presente, se elimina; si no está, se agrega
+    sideMenu.classList.add('active');
+});
+
+document.getElementById('cerrar').addEventListener('click', function() {
+    // Obtiene el elemento del menú lateral con el ID 'side-menu'
+    var sideMenu = document.getElementById('side-menu');
+    sideMenu.classList.add('disabled');
+});
