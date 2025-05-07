8e03835ca2e8a257b2585b54a96e34b2f4ddb6b3
eve1727 <evelynesparza23@cbtis52.edu.mx>
2025-05-06 22:26:44 -0600

    inicie el archivo tdah y organice archivos

---
+++ js/tdah.js
@@ -0,0 +1,17 @@
+// Obtiene el botón con el ID 'hamburger-btn' y agrega un evento de escucha al hacer clic
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+
+    // Obtiene el elemento del menú lateral con el ID 'side-menu'
+    var sideMenu = document.getElementById('side-menu');
+
+    // Alterna la clase 'active' en el menú lateral
+    // Si la clase 'active' está presente, se elimina; si no está, se agrega
+    sideMenu.classList.add('active');
+    sideMenu.classList.remove('disabled');
+});
+
+document.getElementById('cerrar').addEventListener('click', function() {
+    // Obtiene el elemento del menú lateral con el ID 'side-menu'
+    var sideMenu = document.getElementById('side-menu');
+    sideMenu.classList.add('disabled');
+});
