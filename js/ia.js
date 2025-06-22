00b43639347a078960fad5fc0e7ef1caad62f9da
eve1727 <evelynesparza23@cbtis52.edu.mx>
2025-06-21 19:46:59 -0600

    Hice cambios en ayuda

--- js/ia.js
+++ js/ia.js
@@ -1,3 +1,22 @@
+// Obtiene el botón con el ID 'hamburger-btn' y agrega un evento de escucha al hacer clic
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+
+  // Obtiene el elemento del menú lateral con el ID 'padecimientos'
+  var padecimientos = document.getElementById('padecimientos');
+
+  // Alterna la clase 'active' en el menú lateral
+  // Si la clase 'active' está presente, se elimina; si no está, se agrega
+  padecimientos.classList.add('active');
+  padecimientos.classList.remove('disabled');
+});
+
+document.getElementById('cerrar').addEventListener('click', function() {
+  // Obtiene el elemento del menú lateral con el ID 'side-menu'
+  var padecimientos = document.getElementById('padecimientos');
+  padecimientos.classList.add('disabled');
+});
+
+
 // Función para cambiar el tamaño de la fuente
 
 function changeFontSize(delta) {
